6505c671c0d8e5e29bb91be31dbce7ca02f6b062
isiis0406 <Issabobobaldepro@gmail.com>
2024-05-30 14:02:29 +0200

    server v1

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import logger from './logger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  app.useLogger(logger);
+  await app.listen(3000);
+  logger.info('Application is running on port 3000');
+}
+bootstrap();
